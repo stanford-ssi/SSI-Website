764187a8aaab705bb2e454fc82cf5019eba6bf7b
Elijah Kim <reachelijah@gmail.com>
2023-08-18 16:41:23 -0700

    Leadership page

---
+++ src/pages/leadership/index.tsx
@@ -0,0 +1,5 @@
+import { LeadershipPage } from 'client/components/leadershipPage';
+
+export default function Page() {
+  return <LeadershipPage />;
+}
---
+++ src/pages/leadership/leaders.tsx
@@ -0,0 +1,24 @@
+import { LeaderInfo } from 'client/components/leadershipTile';
+
+export const leaders: LeaderInfo[] = [
+  {
+    name: 'Elijah Kim',
+    position: 'Co-President',
+    img: '/images/elijah.jpg',
+    email: 'elijahkim@stanford.edu',
+    linkedIn: 'https://www.linkedin.com/in/elijah-kim/',
+    twitter: 'https://twitter.com/bimikit'
+  },
+  {
+    name: 'Angelina Krinos',
+    position: 'Co-President',
+    img: '/images/angelina.jpg',
+    email: 'akrinos@stanford.edu'
+  },
+  {
+    name: 'Lawton Skaling',
+    position: 'Operations Lead',
+    img: '/images/lawton.jpg',
+    email: 'lskaling@stanford.edu'
+  }
+];

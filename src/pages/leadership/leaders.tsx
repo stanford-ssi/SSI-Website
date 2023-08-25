4fd28a1a5a054a7b6227eb3b45d888a0d35d5a3b
Elijah Kim <reachelijah@gmail.com>
2023-08-24 17:26:27 -0700

    Add people to leadership page

--- src/pages/leadership/leaders.tsx
+++ src/pages/leadership/leaders.tsx
@@ -20,5 +20,26 @@ export const leaders: LeaderInfo[] = [
     position: 'Operations Lead',
     img: '/images/lawton.jpg',
     email: 'lskaling@stanford.edu'
+  },
+  {
+    name: 'Will Neal-Boyd',
+    position: 'Mars Co-Lead',
+    img: '/images/will.jpg',
+    email: 'willnb@stanford.edu',
+    linkedIn: 'https://www.linkedin.com/in/will-neal-boyd-138095242/'
+  },
+  {
+    name: 'Evelyn Nutt',
+    position: 'Rockets Co-Lead',
+    img: '/images/evelyn.jpg',
+    email: 'evelynan@stanford.edu',
+    linkedIn: 'https://www.linkedin.com/in/evelyn-nutt-28a0661b1'
+  },
+  {
+    name: 'Jay (Jonathan) Siskind',
+    position: 'Mars Co-Lead',
+    img: '/images/jay.jpg',
+    email: 'jsiskind@stanford.edu',
+    linkedIn: 'https://www.linkedin.com/in/jonathansiskind/'
   }
 ];

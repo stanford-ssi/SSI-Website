33216efe10e0ba2292ced835b978afcfd65f9a24
AWESDUDEtheCOOL <56030852+AWESDUDEtheCOOL@users.noreply.github.com>
2024-06-24 20:14:08 -0400

    font experiments 1

--- src/client/theme.ts
+++ src/client/theme.ts
@@ -1,4 +1,15 @@
 import { extendTheme } from '@chakra-ui/react';
+import { Inter, Roboto_Mono } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap'
+});
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap'
+});
 
 export const theme = extendTheme({
   styles: {

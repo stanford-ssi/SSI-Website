286ed6d1a7747d67751215ec505f1ebc2c432bbc
AWESDUDEtheCOOL <56030852+AWESDUDEtheCOOL@users.noreply.github.com>
2024-08-13 17:22:59 -0400

    updated leaders 1

--- src/client/components/leaders.tsx
+++ src/client/components/leaders.tsx
@@ -5,7 +5,8 @@ export const leaders: LeaderInfo[] = [
     name: 'Michelle Park',
     position: 'Co-President',
     img: '/images/michelle.jpg',
-    email: 'michpark@stanford.edu'
+    email: 'michpark@stanford.edu',
+    linkedIn: 'https://www.linkedin.com/in/michellepark314/'
   },
   {
     name: 'Ashley Raigosa',
@@ -15,104 +16,100 @@ export const leaders: LeaderInfo[] = [
     linkedIn: 'https://www.linkedin.com/in/ashleyraigosa/'
   },
   // {
-  //   name: 'Elijah Kim',
-  //   position: 'Co-President',
-  //   img: '/images/elijah.jpg',
-  //   email: 'elijahkim@stanford.edu',
-  //   linkedIn: 'https://www.linkedin.com/in/elijah-kim/',
-  //   twitter: 'https://twitter.com/bimikit'
+  //   name: 'Lawton Skaling',
+  //   position: 'Operations Lead',
+  //   img: '/images/lawton.jpg',
+  //   email: 'lskaling@stanford.edu',
+  //   linkedIn: 'http://linkedin.com/in/lskaling',
+  //   instagram: 'https://www.instagram.com/lawton.9o7/',
+  //   twitter: 'https://twitter.com/LawtonSkaling',
+  //   youtube: 'https://www.youtube.com/@lskaling'
   // },
   // {
-  //   name: 'Angelina Krinos',
-  //   position: 'Co-President',
-  //   img: '/images/angelina.jpg',
-  //   email: 'akrinos@stanford.edu',
-  //   linkedIn: 'https://www.linkedin.com/in/angelina-krinos/'
+  //   name: 'Evelyn Nutt',
+  //   position: 'Rockets Co-Lead',
+  //   img: '/images/evelyn.jpg',
+  //   email: 'evelynan@stanford.edu',
+  //   linkedIn: 'https://www.linkedin.com/in/evelyn-nutt-28a0661b1'
   // },
   {
-    name: 'Lawton Skaling',
-    position: 'Operations Lead',
-    img: '/images/lawton.jpg',
-    email: 'lskaling@stanford.edu',
-    linkedIn: 'http://linkedin.com/in/lskaling',
-    instagram: 'https://www.instagram.com/lawton.9o7/',
-    twitter: 'https://twitter.com/LawtonSkaling',
-    youtube: 'https://www.youtube.com/@lskaling'
+    name: 'Houjun Liu',
+    position: 'Co-Lead, Mars',
+    img: '/images/houjun.jpg',
+    email: 'houjun@stanford.edu',
+    linkedIn: 'https://www.linkedin.com/in/houjun-liu',
+    twitter: '',
+    instagram: '',
+    youtube: ''
   },
+  // {
+  //   name: 'Will Neal-Boyd',
+  //   position: 'Mars Co-Lead',
+  //   img: '/images/will.jpg',
+  //   email: 'willnb@stanford.edu',
+  //   linkedIn: 'https://www.linkedin.com/in/will-neal-boyd-138095242/'
+  // },
+  // {
+  //   name: 'Jay (Jonathan) Siskind',
+  //   position: 'Mars Co-Lead',
+  //   img: '/images/jay.jpg',
+  //   email: 'jsiskind@stanford.edu',
+  //   linkedIn: 'https://www.linkedin.com/in/jonathansiskind/'
+  // },
   {
-    name: 'Evelyn Nutt',
-    position: 'Rockets Co-Lead',
-    img: '/images/evelyn.jpg',
-    email: 'evelynan@stanford.edu',
-    linkedIn: 'https://www.linkedin.com/in/evelyn-nutt-28a0661b1'
-  },
-  {
-    name: 'Will Neal-Boyd',
-    position: 'Mars Co-Lead',
-    img: '/images/will.jpg',
-    email: 'willnb@stanford.edu',
-    linkedIn: 'https://www.linkedin.com/in/will-neal-boyd-138095242/'
-  },
-  {
-    name: 'Jay (Jonathan) Siskind',
-    position: 'Mars Co-Lead',
-    img: '/images/jay.jpg',
-    email: 'jsiskind@stanford.edu',
-    linkedIn: 'https://www.linkedin.com/in/jonathansiskind/'
-  },
-  {
-    name: 'Theo Makler',
+    name: 'Sage Wu',
     position: 'Satellites Co-Lead',
-    img: '/images/theo.jpg',
-    email: 'theo24@stanford.edu',
-    linkedIn: 'https://www.linkedin.com/in/theo-makler-25107421a/',
-    instagram: 'https://www.instagram.com/theo.makler/'
+    img: '/images/sage.jpg',
+    email: 'sagewu@stanford.edu',
+    linkedIn: 'https://www.linkedin.com/in/sage-wu/'
   },
   {
-    name: 'Michael Yu',
-    position: 'Balloons Co-Lead',
-    img: '/images/michael.jpg',
-    email: 'mjyu@stanford.edu',
-    linkedIn: 'https://www.linkedin.com/in/michael-yu-ab5800120/'
+    name: 'Niklas Vainio',
+    position: 'Satellites Co-Lead',
+    img: '/images/Niklas.png',
+    email: 'uremail@stanford.edu',
+    linkedIn: 'https://www.linkedin.com/in/niklas-vainio-a4666a225/',
+    instagram: 'https://www.instagram.com/niklas.vainio/'
   },
+  // {
+  //   name: 'Michael Yu',
+  //   position: 'Balloons Co-Lead',
+  //   img: '/images/michael.jpg',
+  //   email: 'mjyu@stanford.edu',
+  //   linkedIn: 'https://www.linkedin.com/in/michael-yu-ab5800120/'
+  // },
+  // {
+  //   name: 'Luis Arizmendi',
+  //   position: 'Balloons Co-Lead',
+  //   img: '/images/luis.jpg',
+  //   email: 'larizmen@stanford.edu',
+  //   linkedIn:
+  //     'https://www.linkedin.com/in/yourlinkedin/luis-arizmendi-6a2381197'
+  // },
   {
-    name: 'Luis Arizmendi',
-    position: 'Balloons Co-Lead',
-    img: '/images/luis.jpg',
-    email: 'larizmen@stanford.edu',
-    linkedIn:
-      'https://www.linkedin.com/in/yourlinkedin/luis-arizmendi-6a2381197'
-  },
-  {
-    name: 'Vassilis Alexopoulos',
-    position: 'Biology Co-Lead',
-    img: '/images/vassilis.jpg',
-    email: 'valex@stanford.edu',
-    linkedIn: 'https://www.linkedin.com/in/valexopa/'
-  },
-  {
-    name: 'Keshav Saigal',
-    position: 'Biology Co-Lead',
-    img: '/images/keshav.jpg',
-    email: 'ksaigal@stanford.edu'
-  },
-  {
-    name: 'James Wall',
-    position: 'Policy Co-Lead',
-    img: '/images/james.jpg',
-    email: 'jameswall@stanford.edu',
-    linkedIn: 'https://www.linkedin.com/in/jamesmgwall/'
-  },
-  {
-    name: 'Matthew Nguyen',
-    position: 'Workspace Manager',
-    img: '/images/matthew.jpg',
-    email: 'mbfly@stanford.edu'
-  },
-  {
-    name: 'Leo Glikbarg',
-    position: 'Safety Lead',
-    img: '/images/leo.jpg',
-    email: 'lglik@stanford.edu'
+    name: 'Ginger Buck',
+    position: 'Co-lead, Astrobio',
+    img: '/images/Ginger.jpg',
+    email: 'ginger24@stanford.edu',
+    linkedIn: 'www.linkedin.com/in/ginger-buck'
   }
+  // {
+  //   name: 'James Wall',
+  //   position: 'Policy Co-Lead',
+  //   img: '/images/james.jpg',
+  //   email: 'jameswall@stanford.edu',
+  //   linkedIn: 'https://www.linkedin.com/in/jamesmgwall/'
+  // },
+  // {
+  //   name: 'Matthew Nguyen',
+  //   position: 'Workspace Manager',
+  //   img: '/images/matthew.jpg',
+  //   email: 'mbfly@stanford.edu'
+  // },
+  // {
+  //   name: 'Leo Glikbarg',
+  //   position: 'Safety Lead',
+  //   img: '/images/leo.jpg',
+  //   email: 'lglik@stanford.edu'
+  // }
 ];
